a1b55a31864c5df98684e20192645aab14a73a25
Jack Harris <st_tuff@icloud.com>
2025-10-16 11:41:43 +1100

    - Added exclude options to prevent coverage running on rouge files.

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,18 @@ export default defineConfig({
         globals: true, // Makes describe, it, expect available globally
         coverage: {
             provider: 'v8', // or 'istanbul'
-            reporter: ['text', 'html', 'json', 'json-summary']
+            reporter: ['text', 'html', 'json', 'json-summary'],
+            exclude: [
+                'dist/**',
+                'node_modules/**',
+                'tests/**',
+                'coverage/**',
+                '*.config.js',
+                '*.config.ts',
+                '.github/**',
+                'scripts/**'
+            ]
         }
+
     }
 });

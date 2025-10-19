7b691ca5c67b2f7ce8bbc60afea2038ac894b164
Jack Harris <st_tuff@icloud.com>
2025-10-19 21:38:29 +1100

    - Added JSDoc types to the unxiToDate and unwrapElement.

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,8 +1,18 @@
+/**
+ * Unwraps an element by replacing it with its child nodes
+ * @param {HTMLElement} element - The element to unwrap
+ * @returns {void}
+ */
 function unwrapElement(element) {
     // Replace the element with its children
-    element.replaceWith(...element.childNodes);
+    element.replaceWith(...Array.from(element.childNodes));
 }
 
+/**
+ * Converts a unix timestamp to a JavaScript Date object
+ * @param {number} unixTimestamp - Unix timestamp in seconds
+ * @returns {Date} JavaScript Date object
+ */
 function unixToDate(unixTimestamp) {
     return new Date(unixTimestamp * 1000);
 }

162a8fb8952284fd489df6fb2d3599a15abd9018
Jack Harris <st_tuff@icloud.com>
2025-10-19 21:55:45 +1100

    - Moved built types to /dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import dts from 'rollup-plugin-dts';
 export default {
     input: './types/index.d.ts',
     output: {
-        file: './types/vera.d.ts',
+        file: './dist/vera.d.ts',
         format: 'es'
     },
     plugins: [dts()]
